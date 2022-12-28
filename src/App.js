93508661c2ff16daefc672e93085bfc7dc537171
Donna-Sayos <Donnasayos@gmail.com>
2022-12-27 21:15:10 -0500

    form added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import "./styles.css";
+
+import Form from "./component/form/Form";
 
 export default function App() {
   return (
-    <div className="container text-center mt-5">
-      Hello World
+    <div className="container mt-5">
+      <Form />
     </div>
   );
 }
