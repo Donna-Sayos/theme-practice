fb4a4d81e3deb20623b2401cf27283460dfa2ca4
Donna-Sayos <Donnasayos@gmail.com>
2022-12-27 23:49:46 -0500

    added an email input focus using useRef when login button is pressed

--- src/component/form/Form.js
+++ src/component/form/Form.js
@@ -1,61 +1,57 @@
-import { IoLogoFacebook, IoLogoGoogle } from "react-icons/io5"
+import { useRef } from "react";
+import { IoLogoFacebook, IoLogoGoogle } from "react-icons/io5";
 import DIAMOND from "../../component/assets/diamond.png";
 
 export default function Form() {
+  const btnRef = useRef(null);
+
   return (
-    <section >
+    <section>
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-12 col-md-8 col-lg-6 col-xl-5">
             <div className="form-container card shadow-lg p-3 mb-5">
               <div className="card-body p-5 text-center">
-                <img src={DIAMOND} width={80} height={60} />
+                <img src={DIAMOND} width={80} height={60} alt="diamond" />
                 <h3 className="mb-5">Sign in</h3>
-
                 <div className="form-outline mb-4">
                   <input
                     type="email"
-                    id="typeEmailX-2"
                     className="form-control form-control-lg"
+                    ref={btnRef}
                   />
                   <label className="form-label" htmlFor="typeEmailX-2">
                     Email
                   </label>
                 </div>
-
                 <div className="form-outline mb-4">
                   <input
                     type="password"
-                    id="typePasswordX-2"
                     className="form-control form-control-lg"
                   />
                   <label className="form-label" htmlFor="typePasswordX-2">
                     Password
                   </label>
                 </div>
-
                 <div className="form-check d-flex justify-content-start mb-4">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="form1Example3"
-                  />
-                  <label className="form-check-label mx-1" htmlFor="form1Example3">
+                  <input className="form-check-input" type="checkbox" />
+                  <label
+                    className="form-check-label mx-1"
+                    htmlFor="form1Example3"
+                  >
                     {" "}
                     Remember password{" "}
                   </label>
                 </div>
-
                 <button
-                  className="btn btn-warning btn-lg btn-block"
+                  className="login btn btn-warning btn-lg"
                   type="submit"
+                  onClick={() => btnRef.current.focus()}
                 >
-                 <b>Login</b>
+                  Login
                 </button>
-
-                <hr className="my-4" />
-
+                <hr className="my-4" />{" "}
+                {/* This creates a line divider between the buttons */}
                 <button
                   className="btn btn-lg btn-block btn-success mb-2 m-1"
                   type="submit"
